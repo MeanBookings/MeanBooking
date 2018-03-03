bbc05dc626e373c56d888ae949cb7f8716769936
Beltran Rengifo <merloc@gmail.com>
2018-03-03 11:30:58 +0100

    FIX folder crash

--- client/src/app/routes.ts
+++ client/src/app/routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
-import { BookingsComponent } from './bookings/bookings.component';
+import { BookingsComponent } from './admin/bookings/bookings.component';
 import { AdminComponent } from './admin/admin.component';
 import { AdminGuardService } from '../services/adminguard.service';
-import { UsersComponent } from './users/users.component';
-import { CalendarComponent } from './calendar/calendar.component';
+import { UsersComponent } from './admin/users/users.component';
+import { CalendarComponent } from './admin/calendar/calendar.component';
 import { UserGuardService } from '../services/userguard.service';
 
 export const routes: Routes = [
