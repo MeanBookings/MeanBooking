48acb7f1fb713ddd90a11e50f87ea13b81739c56
Beltran Rengifo <merloc@gmail.com>
2018-03-03 11:22:49 +0100

    folder reorganization

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -16,26 +16,26 @@ import { registerLocaleData } from '@angular/common';
 import localeEs from '@angular/common/locales/es';
 registerLocaleData(localeEs);
 //components
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './header/login/login.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
-import { SignupComponent } from './signup/signup.component';
-import { LogoutComponent } from './logout/logout.component';
+import { SignupComponent } from './header/signup/signup.component';
+import { LogoutComponent } from './header/logout/logout.component';
 import { ProfileComponent } from './profile/profile.component';
-import { BookingsComponent } from './bookings/bookings.component';
+import { BookingsComponent } from './admin/bookings/bookings.component';
 import { AdminComponent } from './admin/admin.component';
-import { UsersComponent } from './users/users.component';
-import { BookingformComponent } from './bookingform/bookingform.component';
+import { UsersComponent } from './admin/users/users.component';
+import { BookingformComponent } from './home/bookingform/bookingform.component';
 //services
 import { BookingService } from '../services/booking.service';
 import { CalendarService } from '../services/calendar.service';
 import { AdminGuardService } from '../services/adminguard.service';
 import { SessionService } from '../services/session.service';
-import { CalendarComponent } from './calendar/calendar.component';
+import { CalendarComponent } from './admin/calendar/calendar.component';
 import { FilterPipe } from '../pipes/filter.pipe';
 import { ArraySortPipe } from '../pipes/order.pipe';
-import { EditDayComponent } from './edit-day/edit-day.component';
+import { EditDayComponent } from './admin/calendar/edit-day/edit-day.component';
 import { UserGuardService } from '../services/userguard.service';
 
 @NgModule({
